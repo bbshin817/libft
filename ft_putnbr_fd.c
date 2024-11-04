6ab0977fdf5f6d87add429c2e96253966996e307
shintaro <bbshintaro0817@gmail.com>
2024-11-05 02:20:15 +0900

    l

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 21:59:00 by user              #+#    #+#             */
-/*   Updated: 2024/10/26 04:59:27 by user             ###   ########.fr       */
+/*   Updated: 2024/10/30 17:35:37 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
+	char	digit;
 
-	str = ft_itoa(n);
-	if (str == NULL)
-		return ;
-	write(fd, str, ft_strlen(str));
-	free(str);
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else
+	{
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			n = -n;
+		}
+		if (n >= 10)
+			ft_putnbr_fd(n / 10, fd);
+		digit = '0' + (n % 10);
+		write(fd, &digit, 1);
+	}
 }
 
 // int	main()
