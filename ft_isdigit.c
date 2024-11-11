b0cdcc72e0407607092fe9d52aaab8d74e683d22
shintaro <bbshintaro0817@gmail.com>
2024-11-11 17:01:56 +0900

    fix

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbaba <sbaba@student.42.fr>                +#+  +:+       +#+        */
+/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 15:27:23 by sbaba             #+#    #+#             */
-/*   Updated: 2024/11/11 16:52:32 by sbaba            ###   ########.fr       */
+/*   Updated: 2024/11/11 17:01:48 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,18 @@ int	ft_isdigit(int c)
 	return ('0' <= c && c <= '9');
 }
 
-#include <stdio.h>
-#include <ctype.h>
+// #include <stdio.h>
+// #include <ctype.h>
 
-int	main(void)
-{
-	int	array[] = {
-		'1', '2', 'a', '&'
-	};
-	int	i = 0;
-	while(i < (sizeof(array) / sizeof(array[0])))
-	{
-		printf("Input: %c\nft_isdigit: %d\nisdigit: %d\n-----\n", array[i], ft_isdigit(array[i]), isdigit(array[i]));
-		i++;
-	}
-}
+// int	main(void)
+// {
+// 	int	array[] = {
+// 		'1', '2', 'a', '&'
+// 	};
+// 	int	i = 0;
+// 	while(i < (sizeof(array) / sizeof(array[0])))
+// 	{
+// 		printf("Input: %c\nft_isdigit: %d\nisdigit: %d\n-----\n", array[i], ft_isdigit(array[i]), isdigit(array[i]));
+// 		i++;
+// 	}
+// }
